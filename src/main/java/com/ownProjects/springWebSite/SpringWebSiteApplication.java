26c2edd177a6d7cf7aa97a82d5cd68aaa0d6b64e
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-09-30 20:06:01 +0200

    creating spring project

---
+++ src/main/java/com/ownProjects/springWebSite/SpringWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ownProjects.springWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSiteApplication.class, args);
+	}
+
+}
